6200795da40bc92378de1c4dc268e2590909c2b4
James Bradbury <jamesbradbury93@gmail.com>
2020-12-02 15:08:08 +0000

    add users update and simplify messaging with metadata

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
 // https://stackoverflow.com/questions/13364243/websocketserver-node-js-how-to-differentiate-clients
 
 const WebSocket = require('ws');
-
 const wss = new WebSocket.Server({ port: 8080 });
 
 let data = {
-  "a" : 220,
-  "b" : 440,
-  "names" : {}
+    "type" : "data",
+    "a" : 1000,
+    "b" : 1001,
 }
 
+let users = {
+    "type" : "users",
+    "connections" : 0
+}
+
+// Unique IDs for each connection
 const s4 = () => {
-  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 }
 
 wss.getID = () => {
-  return s4() + s4() + '-' + s4();
+    return s4() + s4() + '-' + s4();
 }
 
 wss.on('connection', (ws) => {
-  ws.send(JSON.stringify(data)); // on connection immediately update
-  ws.id = wss.getID();
-  ws.on('message', (message) => {
-    // Update internal data structure here
-    let d = message.split(',')
-    let key = d[0]
-    if (key === 'name') {
-      let val = d[1]  
-      data.names[ws.id] = val
-    } else {
-      let val = parseInt(d[1])
-      data[key] = val
-    }
-
-    
-    // Broadcast to everyone else
+    users["connections"] = users.connections + 1;
+    // Update every user about the new connection
     wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(data));
-      }
-    });
-  });
-});
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(users))
+        }
+    })
+    ws.send(JSON.stringify(data)); // on connection immediately update
+    ws.id = wss.getID();
+
+    // When we get a message from a client
+    ws.on('message', function(message){
+        // Update internal data structure here
+        let d = message.split(',')
+        let key = d[0]
+        let val = parseInt(d[1])
+        data[key] = val
+        // Broadcast data to everyone else on slider changes
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(data));
+            }
+        });
+    })
+
+})
