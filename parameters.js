422056ab0bd59c5a1b53f43c3566461cea95c59a
James Bradbury <jamesbradbury93@gmail.com>
2021-02-18 22:13:27 +0000

    add base parameters

---
+++ parameters.js
@@ -0,0 +1,52 @@
+module.exports = {
+    base: {
+        snare : {
+            frequency : 5000,
+            attack: 0.01,
+            decay: 0.498,
+            sustain: 0.001,
+            release: 0.49
+        },
+        kick : {
+            frequency : 43,
+            octaves : 6,
+            attack : 0.001,
+            decay : 0.05,
+            sustain : 1.0,
+            release : 1.25
+        },
+        metal_one : {
+            frequency : 28,
+            harmonicity : 5.1,
+            modulationIndex : 20.41,
+            resonance : 3753,
+            octaves : 2.75,
+        },
+        metal_two : {
+            frequency : 5000,
+            harmonicity : 0.2,
+            modulationIndex : 22.21,
+            resonance : 5000,
+            octaves : 0,
+        },
+        tomLow : {
+            frequency : 54.23,
+            octaves : 7,
+            pitchDecay : 0.05,
+            attack : 0.0,
+            decay : 0.15,
+            sustain : 0.001,
+            release : 0.9
+        },
+        tomHi : {
+            frequency : 639.75,
+            octaves : 1.0,
+            pitchDecay : 0.001,
+            attack : 0.001,
+            decay : 0.6,
+            sustain : 0.01,
+            release : 1.0
+        },
+
+    }
+}
