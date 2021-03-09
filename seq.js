c803bc159ed72d894c82bc22a81f6ee989b1b28e
James Bradbury <jamesbradbury93@gmail.com>
2021-03-09 11:41:04 +0000

    roombased data inside global JSON object

--- seq.js
+++ seq.js
@@ -24,114 +24,174 @@ if (env == 'live') {
     console.log('Booting HTTP Server')
 };
 
-let backend = io(server, 
-	{
+let backend = io(server, {
 		cors: {
 			origin: true, 
 			methods: ['GET', 'PATCH', 'PUT', 'POST']
 		}
 	}
 );
-let numUsers = 0;
-
-let chat = [];
 
 const numInstruments = 6;
 const numSteps = 16;
 
-// Grid
-let grid = []; // 4 instruments
+let blankGrid = new Array(numInstruments)
+    .fill(
+        new Array(numSteps)
+            .fill(false)
+    );
 
-for (let i=0; i < numInstruments; i++) {
-    grid.push([]);
-    for (let j=0; j < numSteps; j++) {
-        grid[i].push(
-            Math.random() > 0.5
-        );
-    }
-}
-
-let play = false;
-let bpm = 120;
-let clockMode = "forward";
-let params = parameters.base;
+let blankEuclid = new Array(numInstruments)
+    .fill(0);
 
 
-backend.on('connection', (socket) => {
-    numUsers += 1;
-    backend.sockets.emit('numUsers', numUsers);
+let grid = {}
+let params = {}
+let clockMode = {}
+let bpm = {};
+let play = {}
+let euclid = {};
+
+let rooms = {};
+let users = {}
+
+const getRoom = id => users[id]
+
+backend.on('connection', socket => {
+
+    socket.on('roomJoin', (room) => {
+        // Check if user is already in a room
+        if (users[socket.id] !== room) {
+            if (socket.id in users) {
+                let prevRoom = getRoom(socket.id)
+                socket.leave(prevRoom)
+                rooms[prevRoom].numUsers -= 1
+            }
+            socket.join(room)
+            // Now log their room in the users database
+            users[socket.id] = room
+    
+            // Does this room already exist?
+            if (rooms.hasOwnProperty(room)) {
+                rooms[room].numUsers += 1
+            } else { // No
+                rooms[room] = {numUsers : 1}
+                grid[room] = blankGrid.map(row => row.map(cell => Math.random() < 0.2))
+                params[room] = parameters.base
+                bpm[room] = 120
+                play[room] = false
+                euclid[room] = new Array(numInstruments).fill(new Array(numSteps).fill(0))
+                clockMode[room] = "forward"
+            }
+            
+            // Update this socket with the new data
+            backend.to(room).emit('bpm', bpm[room]);
+            backend.to(room).emit('play', play[room]);
+            backend.to(room).emit('grid', grid[room]);
+            backend.to(room).emit('clock::mode', clockMode[room]);
+            backend.to(room).emit('params', params[room]);
+            backend.to(room).emit('numUsers', rooms[room].numUsers)
+            backend.to(room).emit('euclid', euclid[room])
+        }
+    })
 
     // When a user disconnects update the number of users
     socket.on('disconnect', (user) => {
-        numUsers -= 1;
-        backend.sockets.emit('numUsers', numUsers);
+        let room = getRoom(socket.id)
+        socket.leave(room)
+        delete users[socket.id]
+        if (room !== undefined) {
+            rooms[room].numUsers -= 1
+            // if (rooms[room].numUsers <= 0) {
+            //     delete rooms[room]
+            //     delete grid[room]
+            //     delete params[room]
+            //     delete bpm[room]
+            //     delete play[room]
+            //     delete euclid[room]
+            // }
+        }
+        console.log(rooms)
     });
-
-    // update each user with the current data
-    socket.emit('bpm', bpm);
-    socket.emit('sync', 0);
-    socket.emit('play', play);
-    socket.emit('grid', grid);
-    socket.emit('chat', chat);
-    socket.emit('clock::mode', clockMode);
-    socket.emit('params', params);
 
     // KICK
     socket.on('params::kick', (parameter, data) => {
-        params.kick[parameter] = data
-        socket.broadcast.emit('params::kick::'+parameter, data)
+        let room = getRoom(socket.id)
+        params[room].kick[parameter] = data
+        socket.to(room).emit('params::kick::'+parameter, data)
     })
 
     // METAL 1
-    socket.on('params::metalOne', (parameter, data) => {
-        params.metalOne[parameter] = data
-        socket.broadcast.emit('params::metalOne::'+parameter, data)
+    socket.on('params::metal1', (parameter, data) => {
+        let room = getRoom(socket.id)
+        params[room].metal1[parameter] = data
+        socket.to(room).emit('params::metal1::'+parameter, data)
     })
 
     // METAL 2
-    socket.on('params::metalTwo', (parameter, data) => {
-        params.metalTwo[parameter] = data
-        socket.broadcast.emit('params::metalTwo::'+parameter, data)
-    })
-
-    // LOW TOM
-    socket.on('params::tomLow', (parameter, data) => {
-        params.tomLow[parameter] = data
-        socket.broadcast.emit('params::tomLow::'+parameter, data)
-    })
-
-    // HIGH TOM
-    socket.on('params::tomHi', (parameter, data) => {
-        params.tomHi[parameter] = data
-        socket.broadcast.emit('params::tomHi::'+parameter, data)
+    socket.on('params::metal2', (parameter, data) => {
+        let room = getRoom(socket.id)
+        params[room].metal2[parameter] = data
+        socket.to(room).emit('params::metal2::'+parameter, data)
     })
 
     // SNARE
     socket.on('params::snare', (parameter, data) => {
-        params.snare[parameter] = data
-        socket.broadcast.emit('params::snare::'+parameter, data)
+        let room = getRoom(socket.id)
+        params[room].snare[parameter] = data
+        socket.to(room).emit('params::snare::'+parameter, data)
+    })
+
+    socket.on('params::fm1', (parameter, data) => {
+        let room = getRoom(socket.id)
+        params[room].fm1[parameter] = data
+        socket.to(room).emit('params::fm1::'+parameter, data)
+    })
+
+    socket.on('params::fm2', (parameter, data) => {
+        let room = getRoom(socket.id)
+        params[room].fm2[parameter] = data
+        socket.to(room).emit('params::fm2::'+parameter, data)
+    })
+
+    socket.on('params::fm3', (parameter, data) => {
+        let room = getRoom(socket.id)
+        params[room].fm3[parameter] = data
+        socket.to(room).emit('params::fm3::'+parameter, data)
+    })    
+    socket.on('params::fm4', (parameter, data) => {
+        let room = getRoom(socket.id)
+        params[room].fm4[parameter] = data
+        socket.to(room).emit('params::fm4::'+parameter, data)
     })
 
     // Now respond to individual clients messages then broadcast changes to every other client
     // Update internal data too
-    socket.on('bpm', (e) => {
-        bpm = e;
-        socket.broadcast.emit('bpm', e);
+    socket.on('bpm', (data) => {
+        let room = getRoom(socket.id)
+        bpm[room] = data;
+        socket.to(room).emit('bpm', data);
     });
-    socket.on('grid', (e) => {
-        grid = e;
-        socket.broadcast.emit('grid', e);
+    socket.on('grid', (data) => {
+        let room = getRoom(socket.id)
+        grid[room] = data;
+        socket.to(room).emit('grid', data);
     });
-    socket.on('play', (e) => {
-        play = e;
-        socket.broadcast.emit('play', e);
-    });
-    socket.on('sync', (e) => {
-        socket.broadcast.emit('sync', e);
+    socket.on('play', (data) => {
+        let room = getRoom(socket.id)
+        play[room] = data
+        socket.to(room).emit('play', data);
     });
 
-    socket.on('clock::mode', (e) => {
-        clockMode = e;
-        socket.broadcast.emit('clock::mode', e);
+    socket.on('clock::mode', (data) => {
+        let room = getRoom(socket.id)
+        clockMode[room] = data;
+        socket.to(room).emit('clock::mode', data);
     }); 
+
+    socket.on('euclid', data => {
+        let room = getRoom(socket.id)
+        euclid[room] = data;
+        socket.to(room).emit('euclid', data)
+    })
 })
