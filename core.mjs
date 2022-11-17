a6f0179c1efd0f12bd8d41686baadd5e860d6b62
James Bradbury <github@jamesbradbury.net>
2022-11-17 01:04:46 +0100

    expose firebase config

--- core.mjs
+++ core.mjs
@@ -19,3 +19,14 @@ export const conformTemplate = (data, constraints) => {
 		return (data >= constraints.min && data <= constraints.max) && data !== null
 	}
 }
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBQ-SUfm2OZ_i4UCSK4qGfqZeflaewS004",
+    authDomain: "intersymmetric-7e851.firebaseapp.com",
+    databaseURL: "https://intersymmetric-7e851-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "intersymmetric-7e851",
+    storageBucket: "intersymmetric-7e851.appspot.com",
+    messagingSenderId: "500933689703",
+    appId: "1:500933689703:web:dd664fc065f7b9cce22fc2",
+    measurementId: "G-Q9HCJJXHGM"
+};
